333911ee8d0ca6bc0e1410d5024487385570802f
martijnkunstman <martijn@kunstman.nl>
2019-10-15 23:58:57 +0200

    sensor

--- public/main.js
+++ public/main.js
@@ -1,9 +1,38 @@
+const captureVideoButton =
+  document.querySelector('#screenshot .capture-button');
+const screenshotButton = document.querySelector('#screenshot-button');
+const img = document.querySelector('#screenshot img');
+const video = document.querySelector('#screenshot video');
+
+const canvas = document.createElement('canvas');
+
+captureVideoButton.onclick = function() {
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+};
+
+screenshotButton.onclick = video.onclick = function() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  // Other browsers will fall back to image/png
+  img.src = canvas.toDataURL('image/webp');
+};
+
+function handleSuccess(stream) {
+  screenshotButton.disabled = false;
+  video.srcObject = stream;
+}
+
+
 //js
 let sensor = new Accelerometer();
 sensor.start();
 
 sensor.onreading = () => {
-    document.getElementById("acceleration_along_X").innerHTML="x:"+sensor.x;
-    document.getElementById("acceleration_along_Y").innerHTML="y:"+sensor.y;
-    document.getElementById("acceleration_along_Z").innerHTML="z:"+sensor.z;
+    document.getElementById("acceleration_along_X").innerHTML=sensor.x;
+    document.getElementById("acceleration_along_Y").innerHTML=sensor.y;
+    document.getElementById("acceleration_along_Z").innerHTML=sensor.z;
 }
+
+sensor.onerror = event => console.log(event.error.name, event.error.message);
