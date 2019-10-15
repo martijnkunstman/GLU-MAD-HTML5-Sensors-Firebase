46fd43df5602218520f5d26b01fe87186882e4ea
martijnkunstman <martijn@kunstman.nl>
2019-10-15 22:54:05 +0200

    debug

--- public/main.js
+++ public/main.js
@@ -3,9 +3,7 @@ let sensor = new Accelerometer();
 sensor.start();
 
 sensor.onreading = () => {
-    document.getElementById("acceleration_along_X").innerHTML=sensor.x;
-    document.getElementById("acceleration_along_Y").innerHTML=sensor.y;
-    document.getElementById("acceleration_along_Z").innerHTML=sensor.z;
+    document.getElementById("acceleration_along_X").innerHTML="x:"+sensor.x;
+    document.getElementById("acceleration_along_Y").innerHTML="y:"+sensor.y;
+    document.getElementById("acceleration_along_Z").innerHTML="z:"+sensor.z;
 }
-
-sensor.onerror = event => console.log(event.error.name, event.error.message);
